507f9af28ecb0c2ec303272d36650b2be03764e4
Markus Lampola <markus.lampola@opr-vakuus.fi>
2018-11-06 09:18:08 +0200

    3.11 puhelinluettelo full stack

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
