e1d8275da3a545deaef6a391308cdea855306a3b
Markus Lampola <markus.lampola@opr-vakuus.fi>
2018-11-14 19:11:36 +0200

    3.21 eriytetty sovelluskehitys- ja tuotantotietokanta

--- models/person.js
+++ models/person.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
 
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+  }
+  
 const MONGO_USER = process.env.MONGO_USER 
 const MONGO_PASS = process.env.MONGO_PASS
 
-const url = `mongodb://${MONGO_USER}:${MONGO_PASS}@ds253783.mlab.com:53783/persons`
+// Production: MONGO_DB = ds253783.mlab.com:53783/persons
+// Development: MONGO_DB = ds111370.mlab.com:11370/markus-db
+const MONGO_DB = process.env.MONGO_DB 
+
+const url = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_DB}`
 
 mongoose.connect(url, { useNewUrlParser: true })
 
